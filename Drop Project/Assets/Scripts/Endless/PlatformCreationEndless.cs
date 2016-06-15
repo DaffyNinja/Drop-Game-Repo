196d07617edea21774cf91a1f9d2b97c1ad9d901
Marcus <mdaygmdesigner@gmail.com>
2016-06-15 15:15:05 +1000

    Made platform creation script spawn up to 6 platforms

--- Drop Project/Assets/Scripts/Endless/PlatformCreationEndless.cs
+++ Drop Project/Assets/Scripts/Endless/PlatformCreationEndless.cs
@@ -16,6 +16,8 @@ public class PlatformCreationEndless : MonoBehaviour
     public float platformCheck;
     public float destroyLimit;
 
+    public Transform platParent;
+
     int platIndex;
     int platNum;
 
@@ -31,7 +33,7 @@ public class PlatformCreationEndless : MonoBehaviour
     {
         PlatformMaintance();
 
-      //  platIndex = Mathf.RoundToInt(Random.Range(0, platformsList.Count));
+        //  platIndex = Mathf.RoundToInt(Random.Range(0, platformsList.Count));
     }
 
     void PlatformMaintance()
@@ -59,13 +61,16 @@ public class PlatformCreationEndless : MonoBehaviour
         Vector3 pos = new Vector3(playerPos.x, playerTrans.position.y - 5, playerPos.z);
         Vector3 pos2 = new Vector3(playerPos.x, playerTrans.position.y - 20, playerPos.z);
         Vector3 pos3 = new Vector3(playerPos.x, playerTrans.position.y - 35, playerPos.z);
+        Vector3 pos4 = new Vector3(playerPos.x, playerTrans.position.y - 50, playerPos.z);
+        Vector3 pos5 = new Vector3(playerPos.x, playerTrans.position.y - 65, playerPos.z);
+        Vector3 pos6 = new Vector3(playerPos.x, playerTrans.position.y - 80, playerPos.z);
 
-        PlatformCreation(pos, pos2, pos3);
+        PlatformCreation(pos, pos2, pos3, pos4, pos5, pos6);
     }
 
 
 
-    void PlatformCreation(Vector3 platformPos1, Vector3 platformPos2, Vector3 platformPos3)     // Instantiates the platforms
+    void PlatformCreation(Vector3 platformPos1, Vector3 platformPos2, Vector3 platformPos3, Vector3 platformPos4, Vector3 platformPos5, Vector3 platformPos6)     // Instantiates the platforms
     {
         bool create = true;
 
@@ -77,13 +82,18 @@ public class PlatformCreationEndless : MonoBehaviour
             {
                 create = false;
             }
+
+            plat.transform.parent = platParent;
         }
 
-        if (create)
+        if (create)   // Make diffrent difficulty platforms spawn 
         {
             Instantiate(platformsList[Mathf.RoundToInt(Random.Range(0, platformsList.Count))], platformPos1, Quaternion.identity);
             Instantiate(platformsList[Mathf.RoundToInt(Random.Range(0, platformsList.Count))], platformPos2, Quaternion.identity);
             Instantiate(platformsList[Mathf.RoundToInt(Random.Range(0, platformsList.Count))], platformPos3, Quaternion.identity);
+            Instantiate(platformsList[Mathf.RoundToInt(Random.Range(0, platformsList.Count))], platformPos4, Quaternion.identity);
+            Instantiate(platformsList[Mathf.RoundToInt(Random.Range(0, platformsList.Count))], platformPos5, Quaternion.identity);
+            Instantiate(platformsList[Mathf.RoundToInt(Random.Range(0, platformsList.Count))], platformPos6, Quaternion.identity);
         }
 
     }
