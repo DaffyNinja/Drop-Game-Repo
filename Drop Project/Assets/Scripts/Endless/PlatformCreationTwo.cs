6e730151c2cbca52296c1ab1fe885b9e5b51bb46
DaffyNinja <marcusday117@gmail.com>
2016-07-01 21:31:49 +1000

    Endless 2
    Changes to track spawn in endless 2

--- Drop Project/Assets/Scripts/Endless/PlatformCreationTwo.cs
+++ Drop Project/Assets/Scripts/Endless/PlatformCreationTwo.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlatformCreationTwo : MonoBehaviour
 {
 
-    public GameObject trackOBJ;
+    public GameObject[] trackOBJ;
 
     public Transform playerTrans;
     Vector3 playerStartPos;
@@ -48,11 +48,11 @@ public class PlatformCreationTwo : MonoBehaviour
     void SpawnPlatforms()  //(UpTo)
     {
         // Positions
-        Vector3 pos = new Vector3(playerStartPos.x + 2f, playerTrans.position.y - 5, playerStartPos.z - 2.5f);
-        Vector3 pos2 = new Vector3(playerStartPos.x + 2f, playerTrans.position.y - 20, playerStartPos.z - 2.5f);
-        Vector3 pos3 = new Vector3(playerStartPos.x + 2f, playerTrans.position.y - 35, playerStartPos.z - 2.5f);
-        Vector3 pos4 = new Vector3(playerStartPos.x + 2f, playerTrans.position.y - 50, playerStartPos.z - 2.5f);
-        Vector3 pos5 = new Vector3(playerStartPos.x + 2f, playerTrans.position.y - 65, playerStartPos.z - 2.5f);
+        Vector3 pos = new Vector3(playerStartPos.x + 5f, playerTrans.position.y - 10, playerStartPos.z);
+        Vector3 pos2 = new Vector3(playerStartPos.x + 5f, playerTrans.position.y - 20, playerStartPos.z);
+        Vector3 pos3 = new Vector3(playerStartPos.x + 5f, playerTrans.position.y - 30, playerStartPos.z);
+        Vector3 pos4 = new Vector3(playerStartPos.x + 5f, playerTrans.position.y - 40, playerStartPos.z);
+        Vector3 pos5 = new Vector3(playerStartPos.x + 5f, playerTrans.position.y - 50, playerStartPos.z);
 
         PlatformCreation(pos, pos2, pos3, pos4, pos5);
 
@@ -71,14 +71,18 @@ public class PlatformCreationTwo : MonoBehaviour
         {
             if (platformPos1.y < t.transform.position.y)
             {
-                print("False");
+                //print("False");
                 create = false;
             }
         }
 
         if (create)
         {
-            Instantiate(trackOBJ, platformPos1, Quaternion.Euler(0, 90, 0));
+            Instantiate(trackOBJ[0], platformPos1, Quaternion.Euler(0, 90, 0));
+            Instantiate(trackOBJ[0], platformPos2, Quaternion.Euler(0, 90, 0));
+            Instantiate(trackOBJ[0], platformPos3, Quaternion.Euler(0, 90, 0));
+            Instantiate(trackOBJ[0], platformPos4, Quaternion.Euler(0, 90, 0));
+            Instantiate(trackOBJ[0], platformPos5, Quaternion.Euler(0, 90, 0));
         }
 
     }
