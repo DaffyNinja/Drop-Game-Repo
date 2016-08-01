868ccaf0e38c5549b4cfc8706fc7492058e77d57
DaffyNinja <marcusday117@gmail.com>
2016-08-01 15:42:11 +1000

    Stasrted work on piston script

---
+++ Drop Project/Assets/Scripts/Endless/PistonsMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PistonsMove : MonoBehaviour
+{
+
+    public float speed;
+
+    public float xPos;
+
+    Vector3 startingPos;
+    Vector3 Movepos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        startingPos = transform.position;
+
+        Movepos = new Vector3(transform.position.x + xPos, transform.position.y, transform.position.z);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.position = Vector3.Lerp(startingPos, Movepos, speed * Time.time);
+
+
+
+    }
+}
